328677fab8ac3489b98cf7590c9ec32653956a28
Alex Xela <alex94prateek@gmail.com>
2023-08-07 07:46:04 +0530

    Replacing with MDS Portfolio

--- script.js
+++ script.js
@@ -1,49 +1,63 @@
-var h1 = document.getElementById("typeIt")
-toPrint = [];
-var next = true;
-var index = -1;
+const body = document.body
 
-for (var i=0; i<h1.children.length; i++) {
-  toPrint.push(h1.children[i].getAttribute("value"))
-};
+const btnTheme = document.querySelector('.fa-moon')
+const btnHamburger = document.querySelector('.fa-bars')
 
-console.log(toPrint);
-
-h1.innerHTML = "";
-
-function typeWriter() {
-  next = false
-  if (i < txt.length) {
-    h1.innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, 100);
-  } else {
-    i = 0;
-    setTimeout(typeDelete, 3000);
-  }
+const addThemeClass = (bodyClass, btnClass) => {
+  body.classList.add(bodyClass)
+  btnTheme.classList.add(btnClass)
 }
 
-function typeDelete() {
-  txt = h1.textContent;
-  if (i < txt.length) {
-    h1.innerHTML = txt.substring(0, txt.length - 1)
-    setTimeout(typeDelete, 100);
-  } else {
-    if (index === (toPrint.length - 1)) {
-      index = -1;
-    };
-    next = true;
-  }
-};
+const getBodyTheme = localStorage.getItem('portfolio-theme')
+const getBtnTheme = localStorage.getItem('portfolio-btn-theme')
 
-function typeNext(){
-  if (next) {
-    index += 1;
-    txt = toPrint[index];
-    typeWriter();
-  }
-};
+addThemeClass(getBodyTheme, getBtnTheme)
 
-var i = 0;
+const isDark = () => body.classList.contains('dark')
 
-setInterval(typeNext, 100);
+const setTheme = (bodyClass, btnClass) => {
+
+	body.classList.remove(localStorage.getItem('portfolio-theme'))
+	btnTheme.classList.remove(localStorage.getItem('portfolio-btn-theme'))
+
+  addThemeClass(bodyClass, btnClass)
+
+	localStorage.setItem('portfolio-theme', bodyClass)
+	localStorage.setItem('portfolio-btn-theme', btnClass)
+}
+
+const toggleTheme = () =>
+	isDark() ? setTheme('light', 'fa-moon') : setTheme('dark', 'fa-sun')
+
+btnTheme.addEventListener('click', toggleTheme)
+
+const displayList = () => {
+	const navUl = document.querySelector('.nav__list')
+
+	if (btnHamburger.classList.contains('fa-bars')) {
+		btnHamburger.classList.remove('fa-bars')
+		btnHamburger.classList.add('fa-times')
+		navUl.classList.add('display-nav-list')
+	} else {
+		btnHamburger.classList.remove('fa-times')
+		btnHamburger.classList.add('fa-bars')
+		navUl.classList.remove('display-nav-list')
+	}
+}
+
+btnHamburger.addEventListener('click', displayList)
+
+const scrollUp = () => {
+	const btnScrollTop = document.querySelector('.scroll-top')
+
+	if (
+		body.scrollTop > 500 ||
+		document.documentElement.scrollTop > 500
+	) {
+		btnScrollTop.style.display = 'block'
+	} else {
+		btnScrollTop.style.display = 'none'
+	}
+}
+
+document.addEventListener('scroll', scrollUp)
